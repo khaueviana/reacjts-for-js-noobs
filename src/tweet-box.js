5171cc921f8d71a914750a13d3b79eb95fda26b6
Khauê Viana <khaueviana@multconnect.com.br>
2016-08-21 22:55:01 -0300

    add upload picture

--- src/tweet-box.js
+++ src/tweet-box.js
@@ -5,10 +5,12 @@ class TweetBox extends Component {
     super(props, context);
 
     this.state = {
-      text: ''
+      text: '',
+      photoAdded: false
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.togglePhoto = this.togglePhoto.bind(this);
 
   };
 
@@ -16,12 +18,56 @@ class TweetBox extends Component {
     this.setState({ text: event.target.value });
   }
 
+  togglePhoto(event) {
+    this.setState({ photoAdded: !this.state.photoAdded });
+  }
+
+  remainingCharacters() {
+    if (this.state.photoAdded) {
+      return 140 - 23 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
+  }
+
+  overflowAlert() {
+     if (this.remainingCharacters() < 0) {
+        
+        var beforeOverflowText, overflowText;
+
+     if (this.state.photoAdded) {
+        beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
+        overflowText = this.state.text.substring(140 - 23);
+     } else {
+        beforeOverflowText = this.state.text.substring(140 - 10, 140);
+        overflowText = this.state.text.substring(140);
+     }
+
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! Too Long:</strong>
+          {beforeOverflowText}
+          <strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="well clearfix">
+      { this.overflowAlert() }
         <textarea className="form-control" onChange={this.handleChange}></textarea>
         <br/>
-        <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
+
+        <button className="btn btn-primary pull-right" disabled={this.remainingCharacters() === 140}>Tweet</button>
+
+        <button className="js-add-photo-button btn btn-default pull-right" onClick={this.togglePhoto}>
+          {this.state.photoAdded ? "✓ Photo Added" : "Add Photo" }
+        </button>
+
+        <span>{ this.remainingCharacters() }</span>
+
       </div>
     );
   }
